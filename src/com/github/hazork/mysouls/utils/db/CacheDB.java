79ccb228575c09298f88fad2f0fad32a58fa1118
Hazork <arthur.fiorette@gmail.com>
2020-12-06 13:46:56 -0300

    Objetos de database modificados e alterados

---
+++ src/com/github/hazork/mysouls/utils/db/CacheDB.java
@@ -0,0 +1,26 @@
+package com.github.hazork.mysouls.utils.db;
+
+public abstract class CacheDB<K, V> extends MapDB<CacheMap<K, V>, K, V> {
+
+    private volatile CacheMap<K, V> cache;
+
+    public CacheDB(long delay, long seconds) {
+	cache = new CacheMap<K, V>(delay, seconds, (k, v) -> save(v));
+    }
+
+    @Override
+    protected CacheMap<K, V> getMap() {
+	return cache;
+    }
+
+    @Override
+    protected void putValue(V value) {
+	cache.put(keyFunction(value), value);
+    }
+
+    protected abstract void save(V value);
+
+    public abstract boolean open();
+
+    public abstract boolean close();
+}
---
+++ src/com/github/hazork/mysouls/utils/db/HashMapDB.java
@@ -0,0 +1,19 @@
+package com.github.hazork.mysouls.utils.db;
+
+import java.util.HashMap;
+
+public abstract class HashMapDB<K, V> extends MapDB<HashMap<K, V>, K, V> {
+
+    private volatile HashMap<K, V> hashmap = new HashMap<>();
+
+    @Override
+    protected HashMap<K, V> getMap() {
+	return hashmap;
+    }
+
+    @Override
+    protected void putValue(V value) {
+	hashmap.put(keyFunction(value), value);
+    }
+
+}
