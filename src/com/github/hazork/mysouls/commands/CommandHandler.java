0044b2bd573140e7f955f21bbc7086dd4bf9fe88
Hazork <arthur.fiorette@gmail.com>
2020-12-09 11:03:27 -0300

    Menu substituiu alguns comandos

--- src/com/github/hazork/mysouls/commands/CommandHandler.java
+++ src/com/github/hazork/mysouls/commands/CommandHandler.java
@@ -3,18 +3,14 @@ package com.github.hazork.mysouls.commands;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.github.hazork.mysouls.commands.commands.GetCoinCommand;
-import com.github.hazork.mysouls.commands.commands.GetSoulCommand;
 import com.github.hazork.mysouls.commands.commands.InfoCommand;
 import com.github.hazork.mysouls.commands.commands.MenuCommand;
-import com.github.hazork.mysouls.commands.commands.SoulsCommand;
 import com.github.hazork.mysouls.data.lang.Lang;
 
 public class CommandHandler implements CommandExecutor {
@@ -27,8 +23,7 @@ public class CommandHandler implements CommandExecutor {
     public CommandHandler(String commandName, String defaultArgument) {
 	this.commandName = commandName;
 	this.defaultArgument = defaultArgument;
-	addCommand(new InfoCommand(), new GetSoulCommand(), new GetCoinCommand(), new MenuCommand(),
-		new SoulsCommand());
+	addCommand(new InfoCommand(), new MenuCommand());
     }
 
     public void registerFor(JavaPlugin plugin) {
@@ -38,7 +33,7 @@ public class CommandHandler implements CommandExecutor {
     private void addCommand(MySoulsCommand... mscs) {
 	Arrays.stream(mscs).forEach(msc -> {
 	    if (commandMap.containsKey(msc.getName()))
-		throw new RuntimeException("Command with this name is already created: " + msc.getName());
+		throw new IllegalArgumentException("Command with this name is already created: " + msc.getName());
 	    else commandMap.put(msc.getName(), msc);
 	});
     }
@@ -47,7 +42,7 @@ public class CommandHandler implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 	String arg0 = args.length == 0 ? defaultArgument : args[0];
 	MySoulsCommand msc = commandMap.get(arg0);
-	if (Objects.nonNull(msc) && msc.predicate(sender)) {
+	if (msc != null && msc.predicate(sender)) {
 	    if (!msc.getPermission().isPresent() || sender.hasPermission(msc.getPermission().get())) {
 		String[] arguments = args.length == 0 ? new String[0] : Arrays.copyOfRange(args, 1, args.length);
 		msc.handle(sender, arguments, label);
