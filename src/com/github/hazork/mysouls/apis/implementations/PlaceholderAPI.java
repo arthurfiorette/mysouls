b13509fa1ce39a46f1d72c57098956871c19822f
Hazork <arthur.fiorette@gmail.com>
2020-12-09 15:35:11 -0300

    PlaceholderAPI Expansion

--- src/com/github/hazork/mysouls/apis/implementations/PlaceholderAPI.java
+++ src/com/github/hazork/mysouls/apis/implementations/PlaceholderAPI.java
@@ -1,7 +1,73 @@
 package com.github.hazork.mysouls.apis.implementations;
 
-import com.github.hazork.mysouls.apis.API;
+import java.util.Arrays;
 
-public class PlaceholderAPI implements API {
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
+import com.github.hazork.mysouls.MySouls;
+import com.github.hazork.mysouls.apis.MySoulsAPI;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+public class PlaceholderAPI extends PlaceholderExpansion implements MySoulsAPI {
+
+    private MySouls mysouls = MySouls.get();
+
+    @Override
+    public boolean canRegister() {
+	return true;
+    }
+
+    @Override
+    public boolean persist() {
+	return true;
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+	return mysouls.getDescription().getAuthors().toString();
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+	return MySouls.NAME;
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+	return MySouls.getVersion();
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, @NotNull String params) {
+	if (player == null) return null;
+	String[] arr = params.split("_");
+	String[] args = Arrays.copyOfRange(arr, 1, arr.length);
+	Object result = null;
+	switch (arr[0]) {
+	    case "soulscount":
+		if (args.length < 1) result = MySouls.getDB().from(player).soulsCount();
+		else {
+		    @SuppressWarnings("deprecation")
+		    OfflinePlayer of = Bukkit.getOfflinePlayer(args[0]);
+		    result = MySouls.getDB().from(player).soulsCount(of.getUniqueId());
+		}
+		break;
+
+	    case "playercount":
+		result = MySouls.getDB().from(player).playerCount();
+		break;
+
+	    case "soulsratio":
+		result = MySouls.getDB().from(player).soulsRatio();
+		break;
+
+	    default:
+		return null;
+	}
+	return result.toString();
+    }
 }
